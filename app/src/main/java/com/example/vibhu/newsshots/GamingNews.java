c99a5f0d47d5da6ed728e1f45826d500306ec4f8
vibhusehra <vibhusehra@gmail.com>
2018-08-24 19:19:58 +0530

    blank activities added

---
+++ app/src/main/java/com/example/vibhu/newsshots/GamingNews.java
@@ -0,0 +1,13 @@
+package com.example.vibhu.newsshots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GamingNews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gaming_news);
+    }
+}
--- app/src/main/java/com/example/vibhu/newsshots/MainActivity.java
+++ app/src/main/java/com/example/vibhu/newsshots/MainActivity.java
@@ -33,9 +33,40 @@ public class MainActivity extends AppCompatActivity {
             cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    openActivity(finali);
                     Log.i("result ","cardView clicked : " + Integer.toString(finali));
                 }
             });
+        }
+    }
+
+    public void openActivity(int i){
+        Intent intent;
+        switch (i){
+            case 0:
+                intent = new Intent(MainActivity.this,SportsNews.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(MainActivity.this,TechNews.class);
+                startActivity(intent);
+                break;
+            case 2:
+                intent = new Intent(MainActivity.this,BusinessNews.class);
+                startActivity(intent);
+                break;
+            case 3:
+                intent = new Intent(MainActivity.this,GamingNews.class);
+                startActivity(intent);
+                break;
+            case 4:
+                intent = new Intent(MainActivity.this,ScienceNews.class);
+                startActivity(intent);
+                break;
+            case 5:
+                intent = new Intent(MainActivity.this,EntertainmentNews.class);
+                startActivity(intent);
+                break;
         }
     }
 }
