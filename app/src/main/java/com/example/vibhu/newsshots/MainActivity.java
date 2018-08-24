de35c05d3fee8294d4a9bc968e41ead34e11cd59
vibhusehra <vibhusehra@gmail.com>
2018-08-24 16:42:50 +0530

    file added

---
+++ app/src/main/java/com/example/vibhu/newsshots/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.vibhu.newsshots;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.widget.GridLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridLayout cardContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardContainer = findViewById(R.id.cardContainer);
+
+        setEvent(cardContainer);
+    }
+
+    private void setEvent(GridLayout cardContainer){
+
+        for(int i=0;i<cardContainer.getChildCount();i++){
+
+            CardView cardView = (CardView) cardContainer.getChildAt(i);
+
+            final int finali = i;
+
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("result ","cardView clicked : " + Integer.toString(finali));
+                }
+            });
+        }
+    }
+}
