c85086210430d905e62493ede0bb0331fdd3a942
vibhusehra <vibhusehra@gmail.com>
2018-09-06 23:14:08 +0530

    Intent added to open the news in browser

--- app/src/main/java/com/example/vibhu/newsshots/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/vibhu/newsshots/RecyclerViewAdapter.java
@@ -1,8 +1,10 @@
 package com.example.vibhu.newsshots;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,7 +39,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull myViewHolder holder, final int position) {
         holder.newsTitle.setText(articles.get(position).getTitle());
         holder.newsDescription.setText(articles.get(position).getDescription());
         String url = articles.get(position).getUrlToImage();
@@ -50,6 +52,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             //Toast.makeText(context,"END",Toast.LENGTH_SHORT).show();
         }
         holder.simpleDraweeView.setImageURI(uri);
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = articles.get(position).getUrl();
+                if(url != null){
+                    Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+                    context.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -61,13 +74,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         TextView newsDescription,newsTitle;
         SimpleDraweeView simpleDraweeView;
+        CardView parentLayout;
 
         public myViewHolder(View itemView) {
             super(itemView);
             newsDescription = itemView.findViewById(R.id.newsDescription);
             newsTitle = itemView.findViewById(R.id.newsTitle);
             simpleDraweeView = itemView.findViewById(R.id.newsImg);
-
+            parentLayout = itemView.findViewById(R.id.parentLayout);
         }
     }
 }
